101aaa4fcbeef0965f9cae6ddd0e2dbf458880e1
Henrique Carrer <hmcarrer@gmail.com>
2026-04-03 19:50:44 -0300

    Add: lru-cache - submission-20

---
+++ Data Structures & Algorithms/lru-cache/submission-20.py
@@ -0,0 +1,22 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.map = OrderedDict()
+        self.capacity = capacity
+        self.filled = 0
+        self.usedStack = []
+
+    def get(self, key: int) -> int:
+        if key not in self.map:
+            return -1
+        self.map.move_to_end(key)
+        return self.map[key]
+
+    def put(self, key: int, value: int) -> None:
+        exists = key in self.map
+        self.map[key] = value
+        if exists:
+            self.map.move_to_end(key)
+        else:
+            if len(self.map) > self.capacity:
+                self.map.popitem(last=False)
