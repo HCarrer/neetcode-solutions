8a97e1fa6a773be1a2fff7855e1dcdda0747d3b7
Henrique Carrer <hmcarrer@gmail.com>
2026-04-05 18:49:59 -0300

    Add: non-cyclical-number - submission-19

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-19.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getSqrSum(self, n: int) -> int:
+        total = 0
+        while n>0:
+            alg = n % 10
+            total+=alg**2
+            n = n // 10
+        return total
+
+    def isHappy(self, n: int) -> bool:
+        ocur = set()
+        while n not in ocur:
+            ocur.add(n)
+            n = self.getSqrSum(n)
+            if n == 1:
+                return True
+        return False
