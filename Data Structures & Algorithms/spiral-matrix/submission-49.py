fcb9a83c3f540b9be9fd32ffb77cb8114cf4bf13
Henrique Carrer <hmcarrer@gmail.com>
2026-04-06 01:29:21 -0300

    Add: spiral-matrix - submission-49

---
+++ Data Structures & Algorithms/spiral-matrix/submission-49.py
@@ -0,0 +1,32 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        # direction = ["r","b","l","t"] #r,b,l,t,r,b,l,t
+        directions = [(0,1), (1,0), (0,-1), (-1,0)] #(x,y)
+        dirIndex = 0
+        visited = 0
+        row=col=0
+        res = []
+        xBounds = [0, len(matrix[0]) - 1]
+        yBounds = [0, len(matrix) - 1]
+        while visited < len(matrix) * len(matrix[0]):
+            res.append(matrix[row][col])
+            visited+=1
+            if dirIndex % 4 == 0:
+                if col == xBounds[1]:
+                    yBounds[0] += 1
+                    dirIndex+=1
+            elif dirIndex % 4 == 1:
+                if row == yBounds[1]:
+                    xBounds[1] -= 1
+                    dirIndex+=1
+            elif dirIndex % 4 == 2:
+                if col == xBounds[0]:
+                    yBounds[1] -= 1
+                    dirIndex+=1
+            elif dirIndex % 4 == 3:
+                if row == yBounds[0]:
+                    xBounds[0] += 1
+                    dirIndex+=1
+            x,y = directions[dirIndex % 4]
+            row, col = row + x, col + y
+        return res
