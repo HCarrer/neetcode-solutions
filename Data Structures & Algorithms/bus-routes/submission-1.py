a7aeea2f9452578cda01b35d6f4225f3ae68a5a9
Henrique Carrer <hmcarrer@gmail.com>
2026-05-11 16:39:28 -0300

    Add: bus-routes - submission-1

---
+++ Data Structures & Algorithms/bus-routes/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        if source == target:
+            return 0
+        
+        adj = {}
+
+        for bus in range(len(routes)):
+            for stop in routes[bus]:
+                if not stop in adj:
+                    adj[stop] = []
+                adj[stop].append(bus)
+
+        q = deque([source])
+        res = 0
+        seenBuses = set()
+        seenStops = set([source])
+        while q:
+            print(q)
+            for _ in range(len(q)):
+                currentStop = q.popleft()
+                if currentStop == target:
+                    return res
+                for bus in adj.get(currentStop, []):
+                    if bus in seenBuses:
+                        continue
+                    seenBuses.add(bus)
+                    for nextStop in routes[bus]:
+                        if nextStop in seenStops:
+                            continue
+                        seenStops.add(nextStop)
+                        q.append(nextStop)
+            res += 1
+
+        return -1
